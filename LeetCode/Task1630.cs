239cb5dc9831f547574006328b7a5cb77ede9ee9
Ivan Zakharov <ibzakharov2@asuproject.ru>
2023-02-05 00:58:48 +0500

    in process

---
+++ LeetCode/Task1630.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode;
+
+public class Task1630
+{
+    public IList<bool> CheckArithmeticSubarrays(int[] nums, int[] l, int[] r)
+    {
+        var res = new List<bool>();
+        for (int i = 0; i < l.Length; i++)
+        {
+            bool check = IsCheck(nums, l[i], r[i]);
+            res.Add(check);
+        }
+
+        return res;
+    }
+
+    public bool IsCheck(int[] nums, int start, int end)
+    {
+        var res = true;
+        var sortNums = new int[end - start + 1];
+        for (int i = start; i <= end; i++)
+        {
+            sortNums[i] = nums[i];
+        }
+        sortNums = sortNums.OrderBy(i => i).ToArray();
+
+        var value = sortNums[1] - sortNums[0];
+        for (int i = 2; i < sortNums.Length; i++)
+        {
+            var diff = sortNums[i] - sortNums[i - 1];
+            if (value != diff)
+            {
+                res = false;
+                break;
+            }
+        }
+
+        return res;
+    }
+}
---
+++ LeetCodeUnitTests/Task1630Test.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeUnitTests;
+
+[TestClass]
+public class Task1630Test
+{
+    [TestMethod]
+    public void CheckArithmeticSubarrays()
+    {
+        var task = new Task1630();
+        var actual = task
+            .CheckArithmeticSubarrays(new[] { 4, 6, 5, 9, 3, 7 }, new[] { 0, 0, 2 }, new[] { 2, 3, 5 });
+
+        var expected = new[] { true, false, true };
+
+        Assert.AreEqual(expected[0], actual[0]);
+        Assert.AreEqual(expected[1], actual[1]);
+        Assert.AreEqual(expected[2], actual[2]);
+    }
+
+    [TestMethod]
+    public void CheckArithmeticSubarrays_01()
+    {
+        var task = new Task1630();
+        var actual = task.CheckArithmeticSubarrays(
+            new[] { -12, -9, -3, -12, -6, 15, 20, -25, -20, -15, -10 },
+            new[] { 0, 1, 6, 4, 8, 7 },
+            new[] { 4, 4, 9, 7, 9, 10 }
+        );
+
+        var expected = new[] { false,true,false,false,true,true };
+
+        Assert.AreEqual(expected[0], actual[0]);
+        Assert.AreEqual(expected[1], actual[1]);
+        Assert.AreEqual(expected[2], actual[2]);
+        Assert.AreEqual(expected[3], actual[3]);
+        Assert.AreEqual(expected[4], actual[4]);
+    }
+}
